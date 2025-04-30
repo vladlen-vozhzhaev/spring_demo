5950bdb65d28affb000b2d81e851b7de1e5b673e
Vlalden <vladlen@vozhzhaev.ru>
2025-04-30 21:31:42 +0300

    add create/read/update

--- src/main/java/com/example/demo/controller/GiftController.java
+++ src/main/java/com/example/demo/controller/GiftController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -38,5 +39,25 @@ public class GiftController {
     public String addGift(@ModelAttribute Gift gift){
         giftRepository.save(gift);
         return "redirect:/hello";
+    }
+    @GetMapping("/gift/{id}")
+    public String showSingleGift(@PathVariable Long id, Model model){
+        Gift gift = giftRepository.findById(id).orElseThrow();
+        model.addAttribute("gift", gift);
+        return "gift-details";
+    }
+    @GetMapping("/edit/{id}")
+    public String showEditGift(@PathVariable Long id, Model model){
+        Gift gift = giftRepository.findById(id).orElseThrow();
+        model.addAttribute("gift", gift);
+        return "edit-gift";
+    }
+    @PostMapping("/edit/{id}")
+    public String updateGift(@PathVariable Long id, @ModelAttribute Gift uploadedGift){
+        Gift gift = giftRepository.findById(id).orElseThrow();
+        gift.setName(uploadedGift.getName());
+        gift.setDescription(uploadedGift.getDescription());
+        giftRepository.save(gift);
+        return "redirect:/gift/"+id;
     }
 }
